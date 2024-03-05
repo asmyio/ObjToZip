99154807de4a09c1d8cf1cbd0ac2afd2db34c73e
Ahmad Siraj MY <28752140+asmyio@users.noreply.github.com>
2024-03-05 21:04:09 +1000

    init: placeholder functions

---
+++ main.py
@@ -0,0 +1,20 @@
+def s3_check():
+    pass
+
+def download_from_s3():
+    pass
+    
+def compress_object_to_zip():
+    pass
+
+def upload_to_s3():
+    pass 
+
+def delete_object_from_s3():
+    pass 
+
+def main(): 
+    pass 
+
+if __name__ == "__main__":
+    main()
